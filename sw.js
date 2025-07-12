dbb8c2e67db55ada37c57c180ba4727bb52aff19
Alecards <alessiocardellini@hotmail.com>
2025-07-12 17:47:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// Aumentata la versione della cache per forzare l'aggiornamento
-const CACHE_NAME = 'alley33-card-v9'; 
+const CACHE_NAME = 'alley33-card-v11'; 
 const urlsToCache = [
   './', 
   './index.html',
@@ -8,23 +7,17 @@ const urlsToCache = [
   './Logo_Alley_bianco.png',
   './icon-192x192.png',
   './icon-512x512.png',
-  // URL del video inserita correttamente
   'https://firebasestorage.googleapis.com/v0/b/alley33-card-v2.firebasestorage.app/o/WelcomeVideo.mp4?alt=media&token=881e2ce2-a3f2-448a-aeed-7308ac905b81'
 ];
 
-// Evento di installazione
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aperta e file aggiunti');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
 });
 
-// Evento di attivazione per pulire le vecchie cache
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -32,7 +25,6 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Cancellazione vecchia cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -41,7 +33,6 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Evento fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
